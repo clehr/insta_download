4ef4225a05c1e3f0d4a0db88bc164801afc02c1d
Christian Lehr <c.lehr@myeosgroup.net>
2020-03-31 15:49:39 +0200

    add react hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const App = () => {
-    return (
-        <div className={"App"}>
-            <div className={"content"}>
-                <h1>Insta Download!</h1>
 
-                <p>Just copy your image/video and it will appear here</p>
-                <input type="text"/>
-                <button>Download</button>
-            </div>
+    React.useEffect(() => console.log("Test"));
 
+    return <div className={"App"}>
+        <div className={"content"}>
+            <h1>Insta Download!</h1>
+
+            <p>Just copy your image/video and it will appear here</p>
+            <input type="text"/>
+            <button>Download</button>
         </div>
-    );
+
+    </div>
 };
 
 export default App;
