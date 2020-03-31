ec8e477dba884f045a25192497bf55081784063f
Christian Lehr <c.lehr@myeosgroup.net>
2020-03-31 15:44:34 +0200

    add basic elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div className={"App"}>
-      <h1>Easy to use!</h1>
-    </div>
-  );
+    return (
+        <div className={"App"}>
+            <div className={"content"}>
+                <h1>Hey to Insta Download!</h1>
+
+                <div className={"input"}>
+                    <section>Just copy your image/video and it will appear here:</section>
+                    <input type="text"/>
+                </div>
+            </div>
+
+        </div>
+    );
 };
 
 export default App;
